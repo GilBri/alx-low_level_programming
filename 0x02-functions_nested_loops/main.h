64dc8b2d4439fc8530b5d28b8c7f1ee3d3d543b9
GilBri <ronoschngr@gmail.com>
2023-02-22 15:01:58 -0800

    change file permission to execute

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-/* This a header file containting declarations 
+/*
+ * This a header file containting declarations
  * for prototype functions used in the 0x02-functions_nested_loops
  * task and directory
  * THis is like creating my own library with declared functions
