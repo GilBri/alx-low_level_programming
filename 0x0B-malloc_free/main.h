7852dc800e2f5fe74c648920b104e334c9823d89
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-03-14 07:29:43 -0700

    update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,16 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* This a header file containing declarations
+/*
+ * This a header file containing declarations
  * for prototype functions defined in the 0x0B-malloc_free 0x0B-malloc_free
  * tasks and directory
  * This is like creating my own library with declared functions
  * The tasks defines  the functions
  */
 
-/*enclosing the files  contentinbetween ifndef,define and endif,		      
-stops the compiler from processing the file twice if its included twice,
-if not so, this will cause an error*/
+/*
+ * enclosing the files  contentinbetween ifndef,define and endif,
+ * stops the compiler from processing the file twice if its included twice,
+ * if not so, this will cause an error
+ */
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
