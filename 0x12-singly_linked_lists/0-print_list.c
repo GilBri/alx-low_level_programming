76679943286d4f810164ecd2debdc9959d03c26c
GilBri <107459269+GilBri@users.noreply.github.com>
2023-03-30 22:31:05 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,11 @@
 #include "lists.h"
-
 #include <stdio.h>
 
 /**
-
  * print_list - prints all the elements of a list_t list
-
  * @h: the list_t list(pointer to the start of the list_t)
-
  *
-
  * Return: number of nodes in h
-
  */
 
 size_t print_list(const list_t *h)
