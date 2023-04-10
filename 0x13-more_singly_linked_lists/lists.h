d9e694b7d84a49cb50819b46f131a70c7cd8c3cc
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-04-10 09:11:53 -0700

    modify data structure

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,10 +16,9 @@
 typedef struct list_s
 
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 
