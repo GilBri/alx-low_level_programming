df6c19d8d72266db71b6b5aa5ae577583204ff46
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-04-10 10:37:42 -0700

    use while statement to implement function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
@@ -10,15 +11,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		nodes++;
 	}
 
-	return (count);
+	return (nodes);
 
 }
