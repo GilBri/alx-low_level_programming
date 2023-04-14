dd750bdada0d76e7657aab28cd550e3c7cc2c870
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-04-14 16:39:24 -0700

    rewrite header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,16 +2,10 @@
 #define MAIN_H
 
 #include <sys/types.h>
-#include <sys/statt.h>
-#include <fcnt1.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-/**
- * Desc: Header file containing prototypes for all functions
- * 	 written in the 0x15-file_io dir.
- */
-
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
