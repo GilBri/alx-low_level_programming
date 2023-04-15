c054baaaa91b64a357d8e3fd8e7893fc18b93293
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-04-14 17:31:44 -0700

    create a function takes in a pointer to a pointer to the head of the list, as well as the integer data to be added to the new node.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to the pointer of the head of the list
+ * @n: data to be added to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
