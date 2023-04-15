a31d671be3e0832b9e9d21301f8facb96027453b
GilBri'git config --global user.email ronoschngr@gmail.comgit config --global user.name "GilBri <ronoschngr@gmail.com>
2023-04-14 18:07:14 -0700

    function takes a pointer to a pointer to the head node of a listint_t linked list as input.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (n);
+}
+
